c0b46bdad7e42c3f5fc4b46824e27294b73fced4
sakakendo <sakakendo0321@gmail.com>
2017-12-30 20:53:10 +0900

    basic auth

--- hello.js
+++ hello.js
@@ -1,3 +1,23 @@
+var page=require('webpage').create();
+page.onResourceReceived=function(responce){
+	console.log("onResourceReceived\nurl:"+responce.url+"\nheaders:"+responce.headers+"\nstatus:"+responce.status);
 
-console.log("hello phantom");
-phantom.exit();
+}
+
+page.settings.userName="XXX";
+page.settings.password="xxx";
+page.open('https://sakakendo.pythonanywhere.com/basic',function(status){
+	console.log("index.html\nStatus "+status);
+	if(status == "success"){
+		page.render('sakakendo.pythonanywhere.png');
+		console.log("title "+page.title);
+		console.log("content "+page.content);
+	}
+	page.evaluate(function(){
+		a=document.getElementByTagName("a");
+		console.log(a+"href"+a.href);
+	});
+
+
+	phantom.exit();
+});
